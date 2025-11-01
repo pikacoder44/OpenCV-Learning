df62c33a50911813c11f15db66b2d933f008c23e
pikacoder44 <pikaop125@gmail.com>
2025-11-01 15:27:23 +0500

    RGB pixel values

--- tut2.py
+++ tut2.py
@@ -2,8 +2,8 @@ import cv2
 
 img = cv2.imread("assets/logo.png", -1)
 
-print(img)  # This will print a numpy array
-print(type(img))  # <class 'numpy.ndarray'>
+# print(img)  # This will print a numpy array
+# print(type(img))  # <class 'numpy.ndarray'>
 
 # Image Shape
 print(img.shape)
@@ -11,3 +11,17 @@ print(img.shape)
 # where 995 is Height(rows) of numpy array
 # where 1000 is Width(cols) of numpy array
 # where 3 is Channels of numpy array
+
+
+# RGB vs BGR
+# Standard is RGB - OpenCV uses BGR (Blue, Green, Red)
+# when we get [10,20,30] as array we get it as:
+# 10 = Blue , 20 = Green, 30 = Red
+
+
+# Accessing Pixel Values
+
+# print(img[0])  # this gives us first row
+# print(img[257][45:400]) # print pixels on 257th row between 45th pixel to 400th pixel
+
+print(img[257][400]) # prints 400th pixel on 257th row
