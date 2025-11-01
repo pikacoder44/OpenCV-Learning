a488076b5b7a6a6e9fda175f8f2374afce6cf831
pikacoder44 <pikaop125@gmail.com>
2025-11-01 13:21:52 +0500

    Rotate Image & Save manipulated Image

--- tut1.py
+++ tut1.py
@@ -9,6 +9,12 @@ img = cv2.imread("assets/logo.png", 1)
 img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 
 
+# Rotate Image:
+img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+
+# Make a new image out of the manipulated one
+cv2.imwrite("new_img.jpg", img)
+
 # Display Image:
 cv2.imshow("Image", img)
 
