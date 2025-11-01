ad08fac041ca0efff5a4adb0ac91323995aef3d1
pikacoder44 <pikaop125@gmail.com>
2025-11-01 13:46:31 +0500

    Image Shape

--- tut2.py
+++ tut2.py
@@ -2,4 +2,12 @@ import cv2
 
 img = cv2.imread("assets/logo.png", -1)
 
-print(img)
+print(img)  # This will print a numpy array
+print(type(img))  # <class 'numpy.ndarray'>
+
+# Image Shape
+print(img.shape)
+# It gives something like: (995,1000,2)
+# where 995 is Height(rows) of numpy array
+# where 1000 is Width(cols) of numpy array
+# where 3 is Channels of numpy array
