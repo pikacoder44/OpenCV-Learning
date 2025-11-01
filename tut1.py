20832296bd412a73c313a08e77c08ee5dc35c1f4
pikacoder44 <pikaop125@gmail.com>
2025-11-01 13:13:00 +0500

    Resize Image

--- tut1.py
+++ tut1.py
@@ -3,10 +3,16 @@ import cv2
 # Load Image in python
 img = cv2.imread("assets/logo.png", 1)
 
+
+# Resize image
+# img = cv2.resize(img, (400, 400))
+img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+
+
 # Display Image:
 cv2.imshow("Image", img)
 
 
 # Close
-cv2.waitKey(0) # waits for 0 seconds
+cv2.waitKey(0)  # waits for 0 seconds
 cv2.destroyAllWindows()
