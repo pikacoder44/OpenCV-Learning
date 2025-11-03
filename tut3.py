7386277f5cf10315e754ef9a789da087cc3327e4
pikacoder44 <pikaop125@gmail.com>
2025-11-03 20:09:36 +0500

    Show webcam/video 4 times in quadrants

--- tut3.py
+++ tut3.py
@@ -1,12 +1,28 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture("assets/video.mp4") # Put 0 in place of video directory, for webcam
+cap = cv2.VideoCapture(
+    "assets/video.mp4"
+)  # Put 0 in place of video directory, for webcam
 
 # Play Untill keypress
 while True:
     ret, frame = cap.read()
-    cv2.imshow("frame", frame)
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+    # Opening a canvas and display video/webcam on that
+    image = np.zeros(frame.shape, np.uint8)  # it makes entire numpy array 0s ie blank
+    #                      ^
+    #           pasting shape of frame
+
+    smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+
+    image[: height // 2, : width // 2] = smaller_frame
+    image[height // 2 :, : width // 2] = smaller_frame
+    image[: height // 2, width // 2 :] = smaller_frame
+    image[height // 2 :, width // 2 :] = smaller_frame
+
+    cv2.imshow("frame", image)
 
     if cv2.waitKey(1) == ord("q"):
         break
