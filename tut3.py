1ee3b8d18558146c1a1054f98150f95f62b4f2e4
pikacoder44 <pikaop125@gmail.com>
2025-11-03 20:32:01 +0500

    tut 3 finished

--- tut3.py
+++ tut3.py
@@ -18,10 +18,9 @@ while True:
     smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
 
     image[: height // 2, : width // 2] = smaller_frame
-    image[height // 2 :, : width // 2] = smaller_frame
-    image[: height // 2, width // 2 :] = smaller_frame
+    image[height // 2 :, : width // 2] = cv2.rotate(smaller_frame, cv2.ROTATE_180)
+    image[: height // 2, width // 2 :] = cv2.rotate(smaller_frame, cv2.ROTATE_180)
     image[height // 2 :, width // 2 :] = smaller_frame
-
     cv2.imshow("frame", image)
 
     if cv2.waitKey(1) == ord("q"):
