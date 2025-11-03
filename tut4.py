04649193599bf1d842c29ee69e32418c198fc649
pikacoder44 <pikaop125@gmail.com>
2025-11-03 20:40:41 +0500

    Draw diagonal single line

--- tut4.py
+++ tut4.py
@@ -9,6 +9,15 @@ while True:
     width = int(cap.get(3))
     height = int(cap.get(4))
 
+    # How to Draw a line
+    img = cv2.line(
+        frame, (0, 0), (width, height), (255, 0, 0), 10
+    )  # It will draw line from top left to shape of frame
+
+    # syntax is: cv2.line(frame, (Starting coordinates), (Ending Coordinates), (RGB of color of line), Thickness of line in px)
+
+    # Coordinates in OpenCV starts from 0,0 which is top left corner of frame
+
     cv2.imshow("frame", frame)
 
     if cv2.waitKey(1) == ord("q"):
