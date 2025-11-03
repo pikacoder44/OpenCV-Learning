c31fd7eae48f79ed5d00fddf48c1dee26d7d424c
pikacoder44 <pikaop125@gmail.com>
2025-11-03 19:55:16 +0500

    Capture webcam or video

--- tut3.py
+++ tut3.py
@@ -1,1 +1,15 @@
-Hi
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture("assets/video.mp4")
+
+# Play Untill keypress
+while True:
+    ret, frame = cap.read()
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
