5221ce2d0eb6fc634b453f74f0e911366bd81208
pikacoder44 <pikaop125@gmail.com>
2025-11-03 19:56:36 +0500

    Added comment for webcam

--- tut3.py
+++ tut3.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture("assets/video.mp4")
+cap = cv2.VideoCapture("assets/video.mp4") # Put 0 in place of video directory, for webcam
 
 # Play Untill keypress
 while True:
