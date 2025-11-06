bcf1b35b5a3761948e251290bf5f4618b3decb1e
pikacoder44 <pikaop125@gmail.com>
2025-11-06 06:48:04 +0500

    Tut 7 - Template Matching

--- tut6.py
+++ tut6.py
@@ -6,11 +6,27 @@ img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
 
 # First we have to convert image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 # Detect Corners
 corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
-# This returns numpy array of floating numbers so we have to convert it to int
-corners = np.int64(corners)
+"""
+    Let’s explain the parts:
 
+        gray → The grayscale image we just made.
+
+        100 → The maximum number of corners to find.
+
+        0.01 → The quality level, meaning how strong a corner should be to count.
+        (1 = only perfect corners, 0.01 = even weak corners)
+
+        10 → The minimum distance (in pixels) between two corners.
+
+        So this line finds up to 100 corners that are at least 10 pixels apart.
+
+
+        This returns numpy array of floating numbers so we have to convert it to int */
+"""
+corners = np.int64(corners)
 for corner in corners:
     x, y = corner.ravel()  # it will do this: [[1,2] , [2,1]]  to [1,2,2,1]
     cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
