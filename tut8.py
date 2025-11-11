cbdbd67de1db8d5977d983391a84f40b3376e4d6
pikacoder44 <pikaop125@gmail.com>
2025-11-11 15:39:59 +0500

    Loading Classifiers

--- tut8.py
+++ tut8.py
@@ -3,13 +3,18 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+#  Loading Classifiers
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
+
+
 while True:
     ret, frame = cap.read()
 
-
-
     cv2.imshow("Frame", cap)
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(1) == ord("q"):
         break
 cv2.releasekey()
 cv2.destroyAllWindows()
